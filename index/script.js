9ab906d42744b24112151eaa949b7cd99873856e
pkkdpr <oliwia.giercza@gmail.com>
2022-10-17 13:42:24 -0500

    stuck on addeventlistener

---
+++ index/script.js
@@ -0,0 +1,11 @@
+const startBtn = document.querySelector(".startGameBtn");
+const canvas = document.getElementById("canvas");
+// const ctx = canvas.getContext("2d");
+// let displayChangeStart = document.querySelector('.start').style.display
+// const start = function() {
+    console.log(startBtn)
+
+  function cons() {
+    console.log('clicked')
+  }
+    startBtn.addEventListener('click', cons())

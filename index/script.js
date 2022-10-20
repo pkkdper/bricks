f1d5b068d22bdd71216ac52ddad9076c7c71ee30
pkkdpr <oliwia.giercza@gmail.com>
2022-10-19 21:50:00 -0500

    ball moving, paddle too, ballwall collision donw, ball paddle collision lacking, cause I don't understand the sin cos method, plus bricks need to be drawn.

--- index/script.js
+++ index/script.js
@@ -2,18 +2,16 @@ const startBtn = document.getElementById("startBtn");
 const restartBtn = document.getElementById("restartBtn");
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
-
-let isGameOver = false;
-let gameId = 0;
+let life = 3;
+// let isGameOver = false;
+// let gameId = 0;
 
 let movePaddleR = false;
 let movePaddleL = false;
 
-let paddleMarginBottom = 30;
-// let paddleX = ctx.width/2 - paddle.width/2;
-// let paddleY = ctx.height - paddle.height-paddleMarginBottom;
-let paddleWidth = 120;
-let paddleHeight = 25;
+const paddleMarginBottom = 30;
+const paddleWidth = 120;
+const paddleHeight = 25;
 
 const paddle = {
   x: canvas.width / 2 - paddleWidth / 2,
@@ -23,28 +21,66 @@ const paddle = {
   dx: 5,
 };
 
-let ballRadius = 19;
-let ballX = ctx.width / 2;
-let ballY = 708;
-let ballSpeedX = 1;
-let ballSpeedY = 1;
-
-let brickX = 1;
-let brickY = 1;
-let brickWidth = 100;
-let brickHeight = 30;
-
-let life = 3;
-
-const bricks = {
-  w: brickWidth,
-  h: brickHeight,
-  offSetLeft: 20,
-  offSetTop: 20,
-  marginTop: 40,
-  x: 45,
-  y: 15,
+const ballRadius = 15;
+const ball = {
+  x: canvas.width / 2,
+  y: paddle.y - ballRadius,
+  r: ballRadius,
+  s: 2,
+  dx: 3,
+  dy: -3,
 };
+
+function drawBall() {
+  ctx.beginPath();
+  ctx.arc(ball.x, ball.y, ball.r, 0, Math.PI * 2);
+  ctx.fillStyle = "yellow";
+  ctx.fill();
+  ctx.strokeStyle = "black";
+  ctx.stroke();
+  ctx.closePath();
+}
+
+
+function ballWall() {
+if(ball.x > canvas.width || ball.x < 0) {
+ball.dx =- ball.dx
+}if(ball.y < 0) {
+  ball.dy =-ball.dy
+}
+if(ball.y + ball.r > canvas.height) {
+  life --;
+  resetBall()
+}
+}
+function resetBall() {
+  ball.x = canvas.width/2
+  ball.y = paddle.y - ballRadius
+  ball.dx = 3*(Math.random()*2-1)
+  ball.dy = -3
+}
+
+// let ballRadius = 19;
+// let ballX = ctx.width / 2;
+// let ballY = 708;
+// let ballSpeedX = 1;
+// let ballSpeedY = 1;
+
+// let brickX = 1;
+// let brickY = 1;
+// let brickWidth = 100;
+// let brickHeight = 30;
+
+
+// const bricks = {
+//   w: brickWidth,
+//   h: brickHeight,
+//   offSetLeft: 20,
+//   offSetTop: 20,
+//   marginTop: 40,
+//   x: 45,
+//   y: 15,
+// };
 // const paddle = {
 //   draw: function () {
 //     ctx.beginPath();
@@ -60,16 +96,11 @@ const bricks = {
 
 function drawPaddle() {
   // ctx.beginPath();
-  ctx.fillStyle = "blue";
-  ctx.fillRect(paddle.x, paddle.y, paddle.width, paddle.height);
+  ctx.fillStyle = "grey";
+  ctx.fillRect(paddle.x, paddle.y, paddle.w, paddle.h);
   // ctx.closePath();
-
-  //colour not working??
-
-  // ctx.fill();
 }
-console.log(paddle.x, paddle.y)
-drawPaddle()
+
 // const ball = {
 //   drawBall: function () {
 //     ctx.beginPath();
@@ -98,8 +129,8 @@ drawPaddle()
 //  if (
 //     ballY > canvas.height - 70 &&
 //     ballY < canvas.height &&
-//     ballX >= paddle1.X &&
-//     ballX <= paddle1.X + 120
+//     ballX >= paddle.X &&
+//     ballX <= paddle.X + 120
 //   ) {
 //     ballSpeedY = ballSpeedY* -1
 
@@ -110,17 +141,60 @@ drawPaddle()
 //     console.log(life);
 //     resetBall();
 // }}
-
-function movePaddle() {
-  if (movePaddleL && paddle1.X > 0) {
-    paddle1.X -= 3;
+document.addEventListener("keydown", (event) => {
+  if (event.code === "ArrowLeft") {
+    movePaddleL = true;
   }
-  if (movePaddleR && paddle1.X < canvas.width - 120) {
-    paddle1.X += 3;
+  if (event.code === "ArrowRight") {
+    movePaddleR = true;
+  }
+});
+document.addEventListener("keyup", (event) => {
+  if (event.code === "ArrowLeft") {
+    movePaddleL = false;
+  } else if (event.code === "ArrowRight") {
+    movePaddleR = false;
+  }
+});
+function movePaddle() {
+  if (movePaddleL && paddle.x > 0) {
+    paddle.x -= 4;
+  }
+  if (movePaddleR && paddle.x + paddle.w < canvas.width) {
+    paddle.x += 4;
   }
 }
 
-// pls explain how it works
+
+function moveBall() {
+  ball.x += ball.dx;
+  ball.y +=ball.dy;
+  // if (ball.x > canvas.width) {
+  //       ballSpeedX = ballSpeedX * -1;
+  //     }
+  //     if (ball.y < 0) {
+  //       ballSpeedY = ballSpeedY * -1;
+  //     }
+  //     if (ball.x < 0) {
+  //       ballSpeedX = ballSpeedX * -1;
+  //     }
+  //    if (
+  //       ball.y > canvas.height - 70 &&
+  //       ball.y < canvas.height &&
+  //       ball.x >= paddle.x &&
+  //       ball.x <= paddle.x+ 120
+  //     ) {
+  //       ballSpeedY = ballSpeedY* -1
+    
+  //     }
+}
+
+function update() {
+  movePaddle();
+  moveBall();
+  ballWall();
+}
+
 // function drawBricks1() {
 //   ctx.beginPath();
 //   // ctx.fillRect(bricks[0][0].x,bricks[0][0].y,bricks[0][0].w,bricks[0][0].h);
@@ -146,56 +220,39 @@ function movePaddle() {
 //   ctx.closePath();
 // }
 
-// function drawBricks() {
-//   for (var i = 0; i < 8; i++) {
-//     for (var j = 0; j < 7; j++) {
-//       ctx.save();
-//       // ctx.fillStyle = "rgb(" + 51 * i + ", " + (255 - 20 * i) + ", 255)";
-//       ctx.translate(10 + j * 130, 10 + i * 50);
-//       ctx.fillRect(45, 15, 100, 30);
-//       ctx.restore();
-//     }
-//   }
-// }
-
-const start = function () {
-  ctx.clearRect(0, 0, canvas.clientWidth, canvas.height);
-  document.querySelector(".start").style.display = "none";
-  document.querySelector(".game").style.display = "block";
-  // drawBricks1();
-  // drawBricks2();
+function start() {
+  ctx.clearRect(0, 0, 500, 400);
+  drawBall();
   drawPaddle();
+  update();
+  requestAnimationFrame(start)
+}
+start()
+// const start = function () {
+//   ctx.clearRect(0, 0, canvas.clientWidth, canvas.height);
+//   document.querySelector(".start").style.display = "none";
+//   document.querySelector(".game").style.display = "block";
+//   // drawBricks1();
+//   // drawBricks2();
+//   drawPaddle();
 
-  // paddle.draw();
-  // ball.drawBall();
-  ballX += ballSpeedX;
-  ballY -= ballSpeedY;
-  // moveBall();
-  // movePaddle();
-  if (isGameOver) {
-    cancelAnimationFrame(gameId);
-    document.querySelector(".game").style.display = "none";
-    document.querySelector(".end").style.display = "block";
-  } else {
-    gameId = requestAnimationFrame(start);
-  }
-};
-startBtn.addEventListener("click", () => {
-  start();
-});
-
-document.addEventListener("keydown", (event) => {
-  if (event.code === "ArrowLeft") {
-    movePaddleL = true;
-  }
-  if (event.code === "ArrowRight") {
-    movePaddleR = true;
-  }
-});
-document.addEventListener("keyup", () => {
-  movePaddleL = false;
-  movePaddleR = false;
-});
+//   // paddle.draw();
+//   // ball.drawBall();
+//   ballX += ballSpeedX;
+//   ballY -= ballSpeedY;
+//   // moveBall();
+//   // movePaddle();
+//   if (isGameOver) {
+//     cancelAnimationFrame(gameId);
+//     document.querySelector(".game").style.display = "none";
+//     document.querySelector(".end").style.display = "block";
+//   } else {
+//     gameId = requestAnimationFrame(start);
+//   }
+// };
+// startBtn.addEventListener("click", () => {
+//   start();
+// });
 
 // const end = function () {
 //   document.querySelector(".game").style.display = "none";
